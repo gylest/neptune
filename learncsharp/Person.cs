90473ddbd543f446d73525428303d66af5546a3c
Tony Gyles <tonygyles@btinternet.com>
2025-09-04 14:21:27 +0100

    Add Nuget package example

---
+++ learncsharp/Person.cs
@@ -0,0 +1,12 @@
+namespace learncsharp
+{
+    using Newtonsoft.Json;
+
+    public class Person
+    {
+        // Add properties and methods for data import functionality here
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateOnly? BirthDate { get; set; }
+    }
+}
--- learncsharp/Program.cs
+++ learncsharp/Program.cs
@@ -2,12 +2,29 @@
 
 using System;
 
-class Program
+
+namespace learncsharp
 {
-    static void Main(string[] args)
+
+    class Program
     {
-        Console.WriteLine("Hello, World!");
-        int age = 845;
-        Console.WriteLine($"Hello, Tony! You are {age} years old.");
+        static void Main(string[] args)
+        {
+            // Basic console output
+            Console.WriteLine("Hello, World!");
+            int age = 845;
+            Console.WriteLine($"Hello, Tony! You are {age} years old.");
+
+            // Data import
+            string projectRoot = AppDomain.CurrentDomain.BaseDirectory;
+            string filePath = Path.Combine(projectRoot, "person_jd.json");
+            
+            // Use an absolute path for testing
+            const string testFile = @"C:\Users\Tony Gyles\source\repos\neptune\learncsharp\person_jd.json";
+            
+            DataImport dataImport = new DataImport();
+            Person? person = dataImport.ImportFile(testFile);
+        }
     }
+
 }
