08ae5de180e18e6ccea51a7d24371ae6c0eef4dc
Tony Gyles <tonygyles@btinternet.com>
2025-11-02 14:28:01 +0000

    Remove DataImport code

--- learncsharp/learncsharp.Dev/Person.cs
+++
@@ -1,9 +0,0 @@
-namespace learncsharp.Dev;
-
-public class Person
-{
-    // Add properties and methods for data import functionality here
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-    public DateOnly? BirthDate { get; set; }
-}
